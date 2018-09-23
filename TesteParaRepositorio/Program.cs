f41741a16346e6f3a9eb74642bc3eac05ca818dc
Matheus Bueno <mwrbueno@gmail.com>
2018-09-23 12:42:28 -0300

    Just a test on windows

---
+++ TesteParaRepositorio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteParaRepositorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('Apenas um teste para o git');
+        }
+    }
+}
